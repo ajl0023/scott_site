986b52a697082f3befedb062c3723202a7d00b7f
ajl0023 <austinlee199723@yahoo.com>
2023-02-24 02:17:49 -0800

    added awards won page

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	export let data;
+	function closeModal() {
+		dispatch('closeModal');
+	}
+</script>
+
+<div
+	class="modal inset-0"
+	on:keydown
+	on:click="{(e) => {
+		if (e.target.classList.contains('modal')) {
+			closeModal();
+		}
+	}}"
+>
+	<div class="modal-content-wrapper p-4">
+		<div class="modal-content">
+			<img class="w-auto max-h-[calc(100vh_-_215px)]" src="{data}" alt="" />
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.modal {
+		position: fixed; /* Fixed position */
+
+		width: 100%; /* Full width */
+
+		background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+		z-index: 9999; /* Set a high z-index */
+		display: flex;
+		justify-content: center;
+
+		align-items: center;
+	}
+</style>
---
+++ src/routes/(content)/awards-won/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Modal from '../../../lib/components/Modal.svelte';
+	import { get_strapi_image_format } from '../../../lib/utils/utils';
+
+	export let data;
+	const images = data['page_data']['images']['data'];
+	let selectedImage = null;
+</script>
+
+<div class="wrapper">
+	<div class="awards-container flex w-full flex-wrap">
+		{#each images as item}
+			<div class="item-wrapper md:basis-4/12 p-5 w-full">
+				<div
+					on:keydown
+					on:click="{() => (selectedImage = get_strapi_image_format(item, 'large'))}"
+					class="item-container border-gray-400 border-[1px] h-full flex items-center cursor-pointer"
+				>
+					<img class="w-full" alt="" src="{get_strapi_image_format(item, 'medium')}" />
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
+{#if selectedImage}
+	<Modal
+        data={selectedImage}
+		on:closeModal="{() => {
+			selectedImage = null;
+		}}"
+	/>
+{/if}
